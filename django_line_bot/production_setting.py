5ef770a48fd9d776b77e5baeea9975280ac5e15a
kaku <richardkuo2002@gmail.comq0>
2022-03-18 22:41:44 +0800

    .

--- django_line_bot/production_setting.py
+++ django_line_bot/production_setting.py
@@ -1,8 +1,14 @@
-import dj_database_url
+
 from .settings import * # 含入原本的settings.py所有設定
 # heroku使用的資料庫為PostgreSQL，所以要修改資料庫設定
 DATABASES = {
-    'default': dj_database_url.config(),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'richard',
+        'USER': 'richard',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost', 
+    }
 }
 STATIC_ROOT = 'staticfiles' # 設定網站正式上線時靜態檔案目錄位置
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # 設定HTTP連線方式
