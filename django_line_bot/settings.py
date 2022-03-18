3aae243820d6b486c94afb7232627fac52c34d8a
kaku <richardkuo2002@gmail.comq0>
2022-03-18 21:24:26 +0800

    .

--- django_line_bot/settings.py
+++ django_line_bot/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'django_line_bot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'richard',
+        'USER': 'richard',
+        'PASSWORD' : '',
+        'HOST': 'localhost',
     }
 }
 
