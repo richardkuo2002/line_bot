2d9ad9e2698b06c39e45b1500fb2e6fd88421a3e
kaku <richardkuo2002@gmail.comq0>
2022-03-18 16:51:43 +0800

    a

--- echobot/views.py
+++ echobot/views.py
@@ -7,7 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import *
+from linebot.models import MessageEvent, TextSendMessage
+
+from echobot.models import *
 
 
 
@@ -33,11 +35,29 @@ def callback(request):
             return HttpResponseBadRequest()
 
         for event in events:
+            # if isinstance(event, MessageEvent):
+            #     line_bot_api.reply_message(
+            #     event.reply_token,
+            #     TextSendMessage(text=event.message.text)
+            #      )
             if isinstance(event, MessageEvent):
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=event.message.text)
-        )
+                mtext=event.message.text
+                uid=event.source.user_id
+                profile=line_bot_api.get_profile(uid)
+                name=profile.display_name
+                pic_url=profile.picture_url
+
+                message=[]
+                if User_Info.objects.filter(uid=uid).exists()==False:
+                    User_Info.objects.create(uid=uid,name=name,pic_url=pic_url,mtext=mtext)
+                    message.append(TextSendMessage(text='會員資料新增完畢'))
+                elif User_Info.objects.filter(uid=uid).exists()==True:
+                    message.append(TextSendMessage(text='已經有建立會員資料囉'))
+                    user_info = User_Info.objects.filter(uid=uid)
+                    for user in user_info:
+                        info = 'UID=%s\nNAME=%s\n大頭貼=%s'%(user.uid,user.name,user.pic_url)
+                        message.append(TextSendMessage(text=info))
+                    line_bot_api.reply_message(event.reply_token,message)
         
         return HttpResponse()
     else:
