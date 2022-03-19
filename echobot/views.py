cec66368d6daa80c34ba67aaf01631874fd13fb1
kaku <richardkuo2002@gmail.comq0>
2022-03-19 23:17:24 +0800

    .

--- echobot/views.py
+++ echobot/views.py
@@ -47,10 +47,11 @@ def callback(request):
                 name=profile.display_name
                 pic_url=profile.picture_url
                 message=[]
-                for t in len(mtext):
-                    if mtext[t] =="幹":
+                for text_check in mtext:
+                    if text_check =="幹":
                         message.append(TextSendMessage(text='⚠由于此讯息不符合符合国家安全法规，已被屏蔽。'))
                         line_bot_api.reply_message(event.reply_token,message)
+                        break
                 if mtext=="510" or mtext=="吳彥霖" or mtext=="彥霖" or mtext=="@Yalin.":
                     message.append(TextSendMessage(text='請支持1號候選人–吳彥霖。資管要贏，票投彥霖！'))
                     line_bot_api.reply_message(event.reply_token,message)
